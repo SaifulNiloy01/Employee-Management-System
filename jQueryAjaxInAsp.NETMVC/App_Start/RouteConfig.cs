9e434016b44194d251c6216f44a752a74ce883cb
SaifulNiloy01 <saifulniloy15@gmail.com>
2018-04-04 01:47:35 +0600

    Third Commit

--- jQueryAjaxInAsp.NETMVC/App_Start/RouteConfig.cs
+++ jQueryAjaxInAsp.NETMVC/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace jQueryAjaxInAsp.NETMVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Login",
+                url: "{controller}/",
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Employee", action = "Index", id = UrlParameter.Optional }
---
+++ jQueryAjaxInAsp.NETMVC/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using jQueryAjaxInAsp.NETMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQueryAjaxInAsp.NETMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(jQueryAjaxInAsp.NETMVC.Models.User userModel)
+        {
+            using (LoginDataBaseEntities db = new LoginDataBaseEntities())
+            {
+                var userDetails = db.Users.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong username or password.";
+                    return View("Index", userModel);
+                }
+                else {
+                    Session["userID"] = userDetails.UserID;
+                    Session["userName"] = userDetails.UserName;
+                    return RedirectToAction("Index", "Employee");
+                }
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            int userId = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("Index","Login");
+        }
+        
+	}
+}
